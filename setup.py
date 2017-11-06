dd28f0bf5ea928163cea96cdcda13c2c5ca6f96e
Edgar Ortiz <edgardoficial.yo@gmail.com>
2017-11-05 21:29:59 -0600

    Modify setup for upload in pypi.org

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ __VERSION__ = '0.1'
 
 
 setup(
-    name='Coralillo',
+    name='Acordeon',
     packages=['acordeon'],
     include_package_data=True,
+    description='A minimal tool for create custom GUI and forms in PyQT4',
     version=__VERSION__
 )
