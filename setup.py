37622a60406f3748c8bb875f13610185e37c87c1
Edgar Ortiz <edgardoficial.yo@gmail.com>
2017-11-05 19:34:00 -0600

    renamed to acordeon

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-
 __VERSION__ = '0.1'
 
 
 setup(
-	name='PyQtForm',
-	packages=['coralillo'],
-	version=__VERSION__
+    name='Coralillo',
+    packages=['acordeon'],
+    include_package_data=True,
+    version=__VERSION__
 )
